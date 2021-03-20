64025a78f65401964c92c6f36b7e206263135875
Alex Bryant <alexanderpbryant@gmail.com>
2021-03-20 17:37:08 -0500

    made the calculator shape

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,38 @@
+function add(a, b) {
+    let c = a + b;
+    return c;
+}
+
+function subtract(a, b) {
+    let c = a - b;
+    return c;
+}
+
+function multiply(a, b) {
+    let c = a * b;
+    return c;
+}
+
+function divide(a, b) {
+    let c = a / b;
+    return c;
+}
+
+function math(operator, numberOne, numberTwo) {
+    switch(operator) {
+        case 'add':
+            add(numberOne, numberTwo);
+            break;
+        case 'subtract':
+            subtract(numberOne, numberTwo);
+            break;
+        case 'multiply':
+            multiply(numberOne, numberTwo);
+            break;
+        case 'divide':
+            divide(numberOne, numberTwo);
+            break;
+    }
+}
+
 
